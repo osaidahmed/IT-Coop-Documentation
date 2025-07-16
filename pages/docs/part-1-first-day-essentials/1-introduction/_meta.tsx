305b06a1556b10cf6821ea2caae8e22f6f3d500a
Omar Saidahmed <omarsaidahmed123@gmail.com>
2025-07-16 11:32:04 -0400

    Update _meta.tsx

--- pages/docs/part-1-first-day-essentials/1-introduction/_meta.tsx
+++ pages/docs/part-1-first-day-essentials/1-introduction/_meta.tsx
@@ -1,5 +1,6 @@
 export default {
   '1-welcome': 'Welcome to the Service Desk',
   '2-accounts-and-credentials': 'Your Accounts & Credentials',
-  '3-critical-passwords': 'Critical Passwords & Access Codes'
+  '3-critical-passwords': 'Critical Passwords & Access Codes',
+  '4-five9': 'Five9'
 };
